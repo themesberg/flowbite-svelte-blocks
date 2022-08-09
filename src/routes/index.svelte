f8e89271ec7c6ab426981a7f9f2d445a3fa67c94
Shinichi Okada <okada.shin@gmail.com>
2022-08-09 15:56:45 +0200

    docs: add Sign up for our newsletter above footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,5 @@
 <script>
-	import {
-		Navbar,
-		NavBrand,
-		NavHamburger,
-		NavUl,
-		NavLi,
-		Button,
-		DarkMode,
-		Footer,
-		FooterBrand,
-		FooterLinkGroup,
-		FooterLink,
-		FooterCopyright,
-		FooterIcon
-	} from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Button, DarkMode } from 'flowbite-svelte';
 	import {
 		AccountRecoverysection,
 		Bannersection,
@@ -42,7 +28,7 @@
 		Testimonialsection,
 		UserOnboarding
 	} from './sections/';
-	import { Github } from './utils';
+
 	let navClass = 'bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800';
 	let navDivClass = 'flex flex-wrap justify-between items-center mx-auto max-w-screen-xl';
 	let btnClass =
@@ -134,75 +120,4 @@
 			</section>
 		</main>
 	</div>
-	<Footer footerType="socialmedia">
-		<div class="grid gap-12 lg:grid-cols-6 lg:gap-24">
-			<div class="col-span-2">
-				<FooterBrand
-					href="https://flowbite.com"
-					src="https://flowbite.com/docs/images/logo.svg"
-					alt="Flowbite Logo"
-					name="Flowbite"
-					aClass="flex mb-6"
-				/>
-				<p class="text-gray-600 dark:text-gray-400">
-					Flowbite is an ecosystem built on top of Tailwind CSS including a component library, block
-					sections, a Figma design system and other resources.
-				</p>
-			</div>
-			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">
-					Resources
-				</h2>
-				<FooterLinkGroup>
-					<FooterLink liClass="mb-4" href="https://flowbite.com/">Flowbite</FooterLink>
-					<FooterLink liClass="mb-4" href="https://tailwindcss.com/">Tailwind CSS</FooterLink>
-					<FooterLink liClass="mb-4" href="https://flowbite-svelte.com/">Flowbite-Svelte</FooterLink
-					>
-				</FooterLinkGroup>
-			</div>
-			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">
-					HELP & SUPPORT
-				</h2>
-				<FooterLinkGroup>
-					<FooterLink liClass="mb-4" href="https://discord.gg/4eeurUVvTy">Discord</FooterLink>
-					<FooterLink
-						liClass="mb-4"
-						href="https://github.com/shinokada/flowbite-svelte-blocks/issues">GitHub</FooterLink
-					>
-				</FooterLinkGroup>
-			</div>
-			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">
-					Follow us
-				</h2>
-				<FooterLinkGroup>
-					<FooterLink liClass="mb-4" href="https://github.com/shinokada/flowbite-svelte-blocks"
-						>Gihub</FooterLink
-					>
-					<FooterLink liClass="mb-4" href="https://discord.gg/4eeurUVvTy">Discord</FooterLink>
-				</FooterLinkGroup>
-			</div>
-			<div>
-				<h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase dark:text-white">Legal</h2>
-				<FooterLinkGroup>
-					<FooterLink
-						liClass="mb-4"
-						href="https://github.com/shinokada/flowbite-svelte-blocks/LICENSE">LICENSE</FooterLink
-					>
-				</FooterLinkGroup>
-			</div>
-		</div>
-		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-		<div class="sm:flex sm:items-center sm:justify-between">
-			<FooterCopyright href="/" by="Flowbite™" />
-			<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-				<FooterIcon
-					href="https://github.com/shinokada/flowbite-svelte-blocks"
-					class="text-gray-400 hover:text-gray-900"
-					icon={Github}
-				/>
-			</div>
-		</div>
-	</Footer>
 </div>
