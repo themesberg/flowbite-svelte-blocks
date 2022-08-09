1779547a11fc3bf1766ad98431dad6970a813066
Shinichi Okada <okada.shin@gmail.com>
2022-08-09 20:34:18 +0200

    fix: update account-recovery

--- src/routes/blocks/account-recovery.svelte
+++ src/routes/blocks/account-recovery.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Section, ForgotPassword, ForgotPasswordHeader } from '$lib';
 	import { ExampleDiv, SectionHeader, EditOnGithub } from '../utils/index';
-	import { Breadcrumb, BreadcrumbItem, Label, Input } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Label, Input, Button, Checkbox } from 'flowbite-svelte';
 	import { Home } from 'svelte-heros';
 </script>
 
@@ -58,9 +58,35 @@
 			</p>
 			<form class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
 				<div>
-					<Label for="email">Your email</Label>
+					<Label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+						>Your email</Label
+					>
 					<Input type="email" name="email" id="email" placeholder="name@company.com" required />
 				</div>
+				<div class="flex items-start">
+					<div class="flex items-center h-5">
+						<input
+							id="terms"
+							aria-describedby="terms"
+							type="checkbox"
+							class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+							required
+						/>
+					</div>
+					<div class="ml-3 text-sm">
+						<label for="terms" class="font-light text-gray-500 dark:text-gray-300"
+							>I accept the <a
+								class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+								href="/">Terms and Conditions</a
+							></label
+						>
+					</div>
+				</div>
+				<Button
+					type="submit"
+					class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+					>Reset password</Button
+				>
 			</form>
 		</ForgotPassword>
 	</Section>
