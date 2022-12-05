8f59bdd5f3bfc66d46390e645a5e419bb0e5cbf7
Shinichi Okada <okada.shin@gmail.com>
2022-12-05 17:45:22 +0900

    fix: Edit on Github links

--- src/routes/marketing/content/+page.svelte
+++ src/routes/marketing/content/+page.svelte
@@ -31,7 +31,7 @@
 			</p>
 			<EditOnGithub
 				size="18"
-				href="https://github.com/shinokada/flowbite-svelte-blocks/blob/main/src/routes/marketing/content.svelte"
+				href="https://github.com/shinokada/flowbite-svelte-blocks/blob/main/src/routes/marketing/content/+page.svelte"
 				iconClass="text-gray-500"
 			/>
 		</div>
