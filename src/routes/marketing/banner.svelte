dcd4e0392fae5b6952d95a29f13dd11d3560ff1a
Shinichi Okada <okada.shin@gmail.com>
2023-03-19 08:11:08 +0900

    fix: add _config.yml, add DocsLink.svelte for read doc

--- src/routes/marketing/banner.svelte
+++ src/routes/marketing/banner.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Section, Banner } from '$lib';
-  import { ExampleDiv, SectionHeader, EditOnGithub, MetaTag } from '../utils';
+  import { ExampleDiv, SectionHeader, EditOnGithub, MetaTag, DocsLink } from '../utils';
   import { Breadcrumb, BreadcrumbItem, CloseButton } from 'flowbite-svelte';
 
   export let visible = true;
@@ -37,6 +37,9 @@
         size="18"
         href="https://github.com/shinokada/flowbite-svelte-blocks/blob/main/src/routes/marketing/banner.svelte"
         iconClass="text-gray-500"
+      />
+      <DocsLink 
+        page={title.toLocaleLowerCase()}
       />
     </div>
   </div>
