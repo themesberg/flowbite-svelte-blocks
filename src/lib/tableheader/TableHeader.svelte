1a2db8d8899a3c4fa3eb2bd754014e8522686df0
Shinichi Okada <okada.shin@gmail.com>
2023-03-23 09:53:59 +0900

    fix: TableHeader add headerType

--- src/lib/tableheader/TableHeader.svelte
+++ src/lib/tableheader/TableHeader.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
   export let divOuterClass: string = 'relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg';
-  export let divInnerClass: string = 'flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4';
+  export let headerType: 'search' | 'textbtn' | 'custom' = 'search'
+  const divInnerClasses = {
+    search: 'flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4',
+    textbtn: 'flex-row items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4',
+    custom: ''
+  };
   export let divSlotClass: string = 'flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3'
 </script>
 
 <div class="{divOuterClass}">
-  <div class="{divInnerClass}">
-    <slot name='search' />
-    <div class="{divSlotClass}">
+  {#if headerType === 'search'}
+    <div class="{divInnerClasses[headerType]}">
+      <slot name='search' />
+      <div class="{divSlotClass}">
+        <slot />
+      </div>
+    </div>
+  {:else}
+    <div class="{divInnerClasses[headerType]}">
       <slot />
     </div>
-  </div>
+  {/if}
 </div>
 
