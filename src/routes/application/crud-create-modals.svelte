daed4cbb704d7ef1152c04e2197aac5afaa0b1d9
Shinichi Okada <okada.shin@gmail.com>
2023-03-24 21:10:18 +0900

    fix: CrudCreateDrawer, crud-create-modals page

--- src/routes/application/crud-create-modals.svelte
+++ src/routes/application/crud-create-modals.svelte
@@ -72,24 +72,24 @@
         <div class="grid gap-4 mb-4 sm:grid-cols-2">
           <div>
             <Label for='name' class="mb-2">Name</Label>
-            <Input type='text' id='name' placeholder='Type product name' />
+            <Input type='text' id='name' placeholder='Type product name' required/>
           </div>
           <div>
             <Label for='brand' class='mb-2'>Brand</Label>
-            <Input type='text' id='brand' placeholder='Product brand' />
+            <Input type='text' id='brand' placeholder='Product brand' required/>
           </div>
           <div>
             <Label for='price' class='mb-2'>Price</Label>
-            <Input type='text' id='price' placeholder='$29999' />
+            <Input type='text' id='price' placeholder='$29999' required/>
           </div>
           <div>
             <Label>Category
-              <Select class="mt-2" items={countries} bind:value={selected} />
+              <Select class="mt-2" items={countries} bind:value={selected} required/>
             </Label>
           </div>
           <div class="sm:col-span-2">
             <Label for="description" class="mb-2">Description</Label>
-            <Textarea id="description" placeholder="Your description here" rows="4" name="description"/>
+            <Textarea id="description" placeholder="Your description here" rows="4" name="description" required />
           </div>
           <Button type='submit' class='w-52'>
             <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
