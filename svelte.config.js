bbd776c0846e531abb7a01e278a189a2bf5ec0ff
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-12 15:17:29 +0100

    feat: sveltekit 2 and dependencies update

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +13,7 @@ const config = {
     preprocess({
       postcss: true
     }),
+    vitePreprocess(),
     mdsvex(mdsvexConfig)
   ],
   vitePlugin: {
